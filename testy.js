de7a238fcfb48055a25d36d7bf3480b5c9a8643b
Lucas <viper20@poczta.fm>
2017-07-25 21:50:45 +0200

    triangle area

---
+++ testy.js
@@ -0,0 +1,5 @@
+var a = prompt('Set height:');
+var h = prompt('Set width:');
+var triangleArea = a * h / 2;
+console.log('Triangle field with base width: ' + a + ' and height ' + h + ' is equal to: ' + triangleArea);
+alert('Triangle area equals ' + triangleArea);
